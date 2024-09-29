3205cdf2a20bf446ed8c8d556a06fa354f54d306
Oscar <oscarportillo84@gmail.com>
2024-09-29 20:56:29 -0300

    Se modifico la estructura del programa, se modifico la funcion mostrar menu

---
+++ Desafio 2/app/menu_app.py
@@ -0,0 +1,44 @@
+from UTN_Heroes_Dataset.utn_funciones import *
+
+from funciones import mostrar_menu
+
+from validaciones import validar_opcion
+
+
+def utn_heroes_app(matriz_heroes):
+    
+    while True:
+
+        mostrar_menu()
+        opcion = validar_opcion(1,13)
+        play_sound()
+
+        match opcion:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
+            case 8:
+                pass
+            case 9:
+                pass
+            case 10: 
+                pass
+            case 11:
+                pass
+            case 12:
+                pass
+            case 13: # Salir del programa
+                break
+            
+        clear_console()
