1caf0bd94820443b016c698172b6c26abc37664c
Oscar <oscarportillo84@gmail.com>
2024-09-29 20:18:01 -0300

    Se modifico la funcion validar y se el agrego documentacion

--- Desafio 2/validaciones/validaciones.py
+++ Desafio 2/validaciones/validaciones.py
@@ -1,7 +1,18 @@
-def validar_opcion(num_min: int, num_max:int):
-    numero = int(input("Ingrese un numero: "))
-    while numero > num_max or numero < num_min:
-        numero = int(input("Error, ingrese un numero valido: "))
-    return int(numero)
+def validar_opcion(num_min: int, num_max: int) -> int:
+    """
+    Funcion que solicita al usuario que ingrese una opción dentro de un rango determinado 
+    y valida que la entrada sea un número válido dentro de ese rango.
+    Args:
+        num_min (int): El valor mínimo para la opción.
+        num_max (int): El valor máximo para la opción.
 
-print(validar_opcion(2, 9))
+    Returns:
+        int: El número entero que el usuario ha ingresado, validado para estar 
+        dentro del rango especificado [num_min, num_max]
+    """
+    opcion = input(f'Ingrese una opcion [{num_min} - {num_max}]:')
+    
+    if not opcion or not opcion.isdigit() or not (num_min <= int(opcion) <= num_max):
+        return validar_opcion(num_min, num_max)
+    
+    return int(opcion)
