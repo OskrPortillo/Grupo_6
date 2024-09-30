874e59533b43a6f746db1ecaa612061b40d14581
Elbasilisco <huamanvillca@gmail.com>
2024-09-29 23:04:02 -0300

    punto 2 y 4

--- Desafio 2/funciones/funciones.py
+++ Desafio 2/funciones/funciones.py
@@ -1,7 +1,6 @@
 from UTN_Heroes_Dataset.utn_matrices import matriz_data_heroes
 
 def info_heroe(indice: int):
-    indice = int(indice)
     info = ""
     for lista in matriz_data_heroes:
         info += (f"{lista[indice]:10}|")
@@ -21,3 +20,23 @@ def utn_mostrar_cantidad_heroes_femeninos(matriz_heroes) -> None:
                 cantidad_femenino += 1
     
     print(f"La cantidad de heroes femeninos es {cantidad_femenino}")
+
+def cant_heroes_masculinos():
+    contador = 0
+    for i in matriz_data_heroes[3]:
+        if i == "Masculino":
+            contador += 1
+    return contador
+
+#info_heroe(0)
+print(cant_heroes_masculinos())
+
+def heroes_mayores_a_160m():
+    contador = 0
+    for i in range (len (matriz_data_heroes[-1])):
+        if matriz_data_heroes[-1][i] > 160:
+            contador += 1
+            info_heroe(i)
+    print(contador)
+
+heroes_mayores_a_160m()
